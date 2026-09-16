4cae561ec4034917c1c5fde07ef3255a19870e9a
ericakaffou <83919016+ericakaffou@users.noreply.github.com>
2026-09-16 23:06:20 +0200

    Update MnoController.java

--- ussd-api-master/src/main/java/com/app/ussd/controller/MnoController.java
+++ ussd-api-master/src/main/java/com/app/ussd/controller/MnoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 import com.app.ussd.dto.MnoDto;
 import com.app.ussd.service.MnoService;
 import io.swagger.annotations.Api;
