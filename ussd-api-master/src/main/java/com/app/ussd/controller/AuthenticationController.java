b4295cddbd614c567fd64779398ce59c001447bf
ericakaffou <83919016+ericakaffou@users.noreply.github.com>
2026-09-17 09:25:24 +0200

    Update AuthenticationController.java

--- ussd-api-master/src/main/java/com/app/ussd/controller/AuthenticationController.java
+++ ussd-api-master/src/main/java/com/app/ussd/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.app.ussd.controller;
 
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
